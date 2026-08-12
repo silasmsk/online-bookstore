8311a3b24c86ae298a2ae5f0862f4e9010263fff
silasmsk <zsimsek@stu.khas.edu.tr>
2026-08-12 21:12:46 +0200

    Add bestseller books to catalog

--- catalog-service/src/main/java/com/bookstore/catalog/DataLoader.java
+++ catalog-service/src/main/java/com/bookstore/catalog/DataLoader.java
@@ -14,8 +14,7 @@ public class DataLoader {
     CommandLineRunner loadBooks(BookRepository bookRepository) {
         return args -> {
 
-            if (bookRepository.count() == 0) {
-
+            if (!bookRepository.existsByTitle("1984")) {
                 bookRepository.save(new Book(
                         "1984",
                         "George Orwell",
@@ -25,7 +24,9 @@ public class DataLoader {
                         8,
                         "https://ik.imagekit.io/tefd900ep/1984.webp"
                 ));
+            }
 
+            if (!bookRepository.existsByTitle("Pride and Prejudice")) {
                 bookRepository.save(new Book(
                         "Pride and Prejudice",
                         "Jane Austen",
@@ -33,9 +34,11 @@ public class DataLoader {
                         10.99,
                         "Classics",
                         5,
-                         "https://ik.imagekit.io/tefd900ep/pride%20and%20prejudice.jpg"
+                        "https://ik.imagekit.io/tefd900ep/pride%20and%20prejudice.jpg"
                 ));
+            }
 
+            if (!bookRepository.existsByTitle("A Brief History of Time")) {
                 bookRepository.save(new Book(
                         "A Brief History of Time",
                         "Stephen Hawking",
@@ -45,7 +48,9 @@ public class DataLoader {
                         6,
                         "https://ik.imagekit.io/tefd900ep/a%20brief%20history%20of%20time.jpg"
                 ));
+            }
 
+            if (!bookRepository.existsByTitle("The Da Vinci Code")) {
                 bookRepository.save(new Book(
                         "The Da Vinci Code",
                         "Dan Brown",
@@ -54,6 +59,66 @@ public class DataLoader {
                         "Mystery",
                         10,
                         "https://ik.imagekit.io/tefd900ep/da%20vinci%20code.jpg"
+                ));
+            }
+
+            if (!bookRepository.existsByTitle("Harry Potter and the Philosopher's Stone")) {
+                bookRepository.save(new Book(
+                        "Harry Potter and the Philosopher's Stone",
+                        "J.K. Rowling",
+                        "A young wizard begins his first year at Hogwarts and discovers a hidden magical world.",
+                        18.99,
+                        "Fantasy",
+                        12,
+                        "https://ik.imagekit.io/tefd900ep/harry%20potter.jpg"
+                ));
+            }
+
+            if (!bookRepository.existsByTitle("Percy Jackson & the Olympians: The Lightning Thief")) {
+                bookRepository.save(new Book(
+                        "Percy Jackson & the Olympians: The Lightning Thief",
+                        "Rick Riordan",
+                        "A modern adventure inspired by Greek mythology, following Percy Jackson and his dangerous quest.",
+                        16.99,
+                        "Fantasy",
+                        10,
+                        "https://ik.imagekit.io/tefd900ep/percy%20jackson.jpg"
+                ));
+            }
+
+            if (!bookRepository.existsByTitle("Fight Club")) {
+                bookRepository.save(new Book(
+                        "Fight Club",
+                        "Chuck Palahniuk",
+                        "A dark novel about identity, consumer culture and an underground fight club.",
+                        14.99,
+                        "Fiction",
+                        7,
+                        "https://ik.imagekit.io/tefd900ep/fight%20club.jpg"
+                ));
+            }
+
+            if (!bookRepository.existsByTitle("Wuthering Heights")) {
+                bookRepository.save(new Book(
+                        "Wuthering Heights",
+                        "Emily Brontë",
+                        "A classic story of passion, revenge and complicated relationships on the Yorkshire moors.",
+                        11.99,
+                        "Classics",
+                        8,
+                        "https://ik.imagekit.io/tefd900ep/wuthering.jpg"
+                ));
+            }
+
+            if (!bookRepository.existsByTitle("Chess")) {
+                bookRepository.save(new Book(
+                        "Chess",
+                        "Stefan Zweig",
+                        "A psychological novella centered on an intense chess match and the lasting effects of isolation.",
+                        9.99,
+                        "Classics",
+                        9,
+                        "https://ik.imagekit.io/tefd900ep/chess.jpg"
                 ));
             }
         };
--- catalog-service/src/main/java/com/bookstore/catalog/repository/BookRepository.java
+++ catalog-service/src/main/java/com/bookstore/catalog/repository/BookRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bookstore.catalog.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    boolean existsByTitle(String title);
+
 }
